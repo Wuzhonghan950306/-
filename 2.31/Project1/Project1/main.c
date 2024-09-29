7bc4838a851b93224769b26530aaf7eb556f9fe5
HW_1 <king10917wu@gmail.com>
2024-09-30 01:01:07 +0800

    Homework_1

---
+++ 2.31/Project1/Project1/main.c
@@ -0,0 +1,26 @@
+﻿// (Table of Squares Cubes) 程式
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("%-8s%-8s%-8s\n", "number", "square", "cube"); // 打印 number square cube 標題且換到下一行
+
+	for (int i = 0; i <= 10; i++)
+	{
+		int num;		// 儲存 number
+		int square_num; // 儲存 number 平方的結果
+		int cube_num;	// 儲存 number 立方的結果
+
+		num = i;
+		square_num = i * i;
+		cube_num = i * i * i;
+
+		printf("%-8d", num);		// 打印 number (數值向左對齊，位數不足補空白)
+		printf("%-8d", square_num); // 打印平方的 number
+		printf("%-8d\n", cube_num); // 打印立方的 number 且換到下一行
+	}
+
+	system("pause");
+	return 0;
+}
---
+++ 2.32/source/main.c
@@ -0,0 +1,39 @@
+// (Largest and Smallest Integers) 程式
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int num1;
+	int num2;
+	int num3;
+
+	printf("請輸入 第一個 整數: ");
+	scanf("%d", &num1);
+
+	printf("請輸入 第二個 整數: ");
+	scanf("%d", &num2);
+
+	printf("請輸入 第三個 整數: ");
+	scanf("%d", &num3);
+
+	if (num1 < num2) {
+		num1 = num1 ^ num2;
+		num2 = num1 ^ num2;
+		num1 = num1 ^ num2;
+	}
+	if (num1 < num3) {
+		num1 = num1 ^ num3;
+		num3 = num1 ^ num3;
+		num1 = num1 ^ num3;
+	}
+	if (num2 < num3) {
+		num2 = num2 ^ num3;
+		num3 = num2 ^ num3;
+		num2 = num2 ^ num3;
+	}
+	printf("%d,num1");
+	printf("%d,num3");
+	system("pause");
+	return 0;
+}
